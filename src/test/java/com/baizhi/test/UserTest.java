e131acb006ccc00c35af4886e9a7e0678dd4d7b7
chenm <15060008054@163.com>
2018-12-24 21:15:24 +0800

    无

--- src/test/java/com/baizhi/test/UserTest.java
+++ src/test/java/com/baizhi/test/UserTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import sun.invoke.empty.Empty;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:spring.xml")
