7fcb9d3f4933cada8f79d539de15d434c6331ad8
chenm <15060008054@163.com>
2018-12-24 20:02:27 +0800

    上传时间：2018年12月24日 20点02分 项目名称：SSM+图书馆(未完成) 修改：新添加了User测试类中的方法showAllUser()

--- src/test/java/com/baizhi/test/UserTest.java
+++ src/test/java/com/baizhi/test/UserTest.java
@@ -1,6 +1,10 @@
 package com.baizhi.test;
 
+import com.baizhi.entity.Result;
+import com.baizhi.service.UserService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -8,6 +12,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration("classpath:spring.xml")
 public class UserTest {
 
+    @Autowired
+    private UserService userService;
 
+    @Test
+    public void showAllUser(){
+        Result code = userService.getCode("12345678912");
+        System.out.println(code);
+    }
 
 }
